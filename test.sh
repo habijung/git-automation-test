7275bd7be39b84013c42d21fbf9ecea1c8efd6d0
HabiJung <habijungx@gmail.com>
2021-09-28 23:20:06 +0900

    Add enter commit message

--- test.sh
+++ test.sh
@@ -4,8 +4,16 @@ git pull origin main
 echo "-> git pull complete"
 
 git add --all
+git status
 echo "-> git add --all complete"
 
-COMMIT_MESSAGE=${1}
+if [ $# -eq 0 ]; then
+	read -p "-> enter commit message : " COMMIT_MESSAGE
+else
+	COMMIT_MESSAGE=${1}
+fi
 git commit -m "${COMMIT_MESSAGE}"
 echo "-> git commit complete"
+
+git push origin HEAD:main
+echo "-> git push complete"
