997226d7ad21a303415a39aa13c65c4bf73a4be9
HabiJung <habijungx@gmail.com>
2021-09-29 00:03:29 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/bash
 
+echo -e "\n-> git pull ...\n"
 git pull origin main
-echo "-> git pull complete"
+echo -e "\n-> git pull complete"
 
+echo -e "-> git add ...\n"
 git add --all
+echo -e "\n-> git add complete"
+
+echo -e "-> git status\n"
 git status
-echo "-> git add --all complete"
+
+echo -e "\n-> git commit ..."
 
 if [ $# -eq 0 ]; then
-	read -p "-> enter commit message : " COMMIT_MESSAGE
+	read -p "-> Enter commit message : " COMMIT_MESSAGE
 else
 	COMMIT_MESSAGE=${1}
 fi
-git commit -m "${COMMIT_MESSAGE}"
-echo "-> git commit complete"
 
+git commit -m "${COMMIT_MESSAGE}"
+echo -e "\n-> git commit complete"
+
+echo -e "-> git push ...\n"
 git push origin HEAD:main
-echo "-> git push complete"
+echo -e "\n-> git push complete\n"
